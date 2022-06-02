15e0a8cff2489467aaed4e94f74c8f7035d1e627
kKar1503 <isonice4572@gmail.com>
2022-06-02 21:53:39 +0800

    refactor: :recycle: Database Config Cleanup
    Code clean up in Database Config

--- discord/src/database/config.js
+++ discord/src/database/config.js
@@ -1,13 +1,14 @@
 import "dotenv/config";
 import { createConnection } from "mysql";
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS } = process.env;
 
 const dbConnection = {
 	getConnection: function () {
 		return createConnection({
-			host: process.env.DB_HOST || "localhost",
-			port: process.env.DB_PORT || 3306,
-			user: process.env.DB_USER,
-			password: process.env.DB_PASS,
+			host: DB_HOST || "localhost",
+			port: DB_PORT || 3306,
+			user: DB_USER,
+			password: DB_PASS,
 			database: "tetralympic",
 			multipleStatements: true,
 		});
