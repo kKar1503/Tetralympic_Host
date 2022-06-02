5f6acd29118f5e06b7bd622d18d06f8b24a3049d
kKar1503 <isonice4572@gmail.com>
2022-06-02 21:48:13 +0800

    feat: :sparkles: Added DiscordUser Model & refactored TetrioUser
    DiscordUser for adding and viewing discord_user. TetrioUser refactor to extend from TetralympicTable

--- discord/src/models/DiscordUser.js
+++ discord/src/models/DiscordUser.js
@@ -1,14 +1,14 @@
 import TetralympicTable from "./TetralympicTable.js";
 
-export default class TetrioUser extends TetralympicTable {
+export default class DiscordUser extends TetralympicTable {
 	constructor(options = {}) {
-		options.tableName = "tetrio_user";
+		options.tableName = "discord_user";
 		super(options);
 	}
 
 	GetUsers() {
 		return new Promise((resolve, reject) => {
-			const queryString = `SELECT id, username, rating, \`rank\`, highest_rank FROM ${this.tableName}`;
+			const queryString = `SELECT id, username, discriminator, fk_tetrio_id FROM ${this.tableName}`;
 			this.connection.query(queryString, (error, result) => {
 				if (error) {
 					if (this.useLogger) this.logger.QueryFailed(queryString, error.code);
--- discord/src/models/index.js
+++ discord/src/models/index.js
@@ -1,1 +1,3 @@
 export { default as TetrioUser } from "./TetrioUser.js";
+export { default as TetralympicTable } from "./TetralympicTable.js";
+export { default as DiscordUser } from "./DiscordUser.js";
