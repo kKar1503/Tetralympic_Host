5883b581729b0c789775cfe7949f6dd4c4451de4
kKar1503 <isonice4572@gmail.com>
2022-06-05 02:15:13 +0800

    feat: :sparkles: Added more Errors Object

--- discord/src/constants/Errors.js
+++ discord/src/constants/Errors.js
@@ -13,32 +13,55 @@ export class DatabaseError extends Error {
 }
 
 export class RegistrationClosedError extends RegistrationError {
+	constructor(deadline, status) {
+		super(`Registration closed, event current status: ${status} (Deadline: ${deadline})`);
+		this.deadline = deadline;
+		this.status = status;
+	}
+}
+
+export class RegistrationNotOpenedError extends RegistrationError {
 	constructor() {
-		super("Registration closed.");
+		super("Registration not opened.");
+	}
+}
+
+export class TournamentIsOverError extends RegistrationError {
+	constructor(eventDate) {
+		super(
+			`Unless you have a time machine, but the event was long over, buddy! (Event Date: ${eventDate})`
+		);
+		this.eventDate = eventDate;
+	}
+}
+
+export class UserUnrankedError extends RegistrationError {
+	constructor() {
+		super("User is unranked.");
 	}
 }
 
 export class OutOfCompetitionLimitError extends RegistrationError {
 	constructor(limit, value, expected) {
+		super(`Out of limit: ${limit} (value: ${value}, expected: ${expected})`);
 		this.limit = limit;
 		this.value = value;
 		this.expected = expected;
-		super(`Out of limit: ${this.limit} (value: ${this.value}, expected: ${this.expected})`);
 	}
 }
 
 export class AlreadyBindedError extends DatabaseError {
 	constructor(username, discordId) {
+		super(`You have already binded with ${username}.`);
 		this.username = username;
 		this.discordId = discordId;
-		super(`You have already binded with ${this.username}.`);
 	}
 }
 
 export class BindedByOthersError extends DatabaseError {
 	constructor(username, discordId) {
+		super(`Someone has already binded with ${username}.`);
 		this.username = username;
 		this.discordId = discordId;
-		super(`Someone has already binded with ${this.username}.`);
 	}
 }
--- discord/src/constants/index.js
+++ discord/src/constants/index.js
@@ -3,8 +3,11 @@ export { RANKS as Ranks } from "./Ranks.js";
 export {
 	RegistrationError,
 	DatabaseError,
+	RegistrationNotOpenedError,
 	RegistrationClosedError,
+	TournamentIsOverError,
 	OutOfCompetitionLimitError,
 	AlreadyBindedError,
 	BindedByOthersError,
+	UserUnrankedError,
 } from "./Errors.js";
