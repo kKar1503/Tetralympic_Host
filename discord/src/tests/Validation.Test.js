15f72a81e49142342a11907e65ebf9b56172a19f
kKar1503 <isonice4572@gmail.com>
2022-06-05 02:16:55 +0800

    test: :white_check_mark: Added tests for Validation

---
+++ discord/src/tests/Validation.Test.js
@@ -0,0 +1,21 @@
+import { Validation } from "../jobs/index.js";
+import { TetrioApi } from "../api/index.js";
+
+let testSubject1 = "jovangq";
+let testSubject2 = "flashm8";
+let testSubject3 = "liak";
+let testSubject4 = "czsmall0402";
+let testSubject5 = "skyllabtw";
+let testResponse = [];
+
+for (let i = 1; i < 11; i++) {
+	Validation(await new TetrioApi().getOneUser(testSubject3), i)
+		.then((c) => (testResponse[i - 1] = `Joined ${i}`))
+		.catch((e) => (testResponse[i - 1] = `Failed ${i}\n${e.message}`));
+}
+
+setTimeout(() => {
+	for (let j = 0; j < testResponse.length; j++) {
+		console.log(testResponse[j]);
+	}
+}, 5000);
