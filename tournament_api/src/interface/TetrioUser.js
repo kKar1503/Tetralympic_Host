87dbf94c63b6292fc90b6b5fad14ec071a585fa3
kKar1503 <isonice4572@gmail.com>
2022-06-05 16:37:53 +0800

    feat: :sparkles: Added options to tetrio user interface
    Option to choosen if to generate interface from API or existing data.

--- tournament_api/src/interface/TetrioUser.js
+++ tournament_api/src/interface/TetrioUser.js
@@ -15,19 +15,30 @@
  * @prop {String} highest_rank
  */
 export default class TetrioUser {
-	constructor(data) {
+	constructor(data, options = {}) {
 		this.id = data._id;
-		this.username = data.username;
+		this.username = data.username.toLowerCase();
 		this.country = data.country;
-		if (data.league) {
-			this.gamesPlayed = data.league.gamesplayed;
-			this.rating = data.league.rating;
-			this.glicko = data.league.glicko;
-			this.rd = data.league.rd;
-			this.rank = data.league.rank;
-			this.apm = data.league.apm;
-			this.pps = data.league.pps;
-			this.vs = data.league.vs;
+		if (options.fromApi) {
+			if (data.league) {
+				this.gamesPlayed = data.league.gamesplayed;
+				this.rating = data.league.rating;
+				this.glicko = data.league.glicko;
+				this.rd = data.league.rd;
+				this.rank = data.league.rank;
+				this.apm = data.league.apm;
+				this.pps = data.league.pps;
+				this.vs = data.league.vs;
+			}
+		} else {
+			this.gamesPlayed = data.gamesplayed;
+			this.rating = data.rating;
+			this.glicko = data.glicko;
+			this.rd = data.rd;
+			this.rank = data.rank;
+			this.apm = data.apm;
+			this.pps = data.pps;
+			this.vs = data.vs;
 		}
 		this.joinDate = new Date(data.ts);
 		this.highest_rank = data.highest_rank;
--- tournament_api/src/interface/index.js
+++ tournament_api/src/interface/index.js
@@ -1,3 +1,4 @@
 export { default as TetrioUserInterface } from "./TetrioUser.js";
 export { default as DiscordUserInterface } from "./DiscordUser.js";
 export { default as CompetitionInterface } from "./Competition.js";
+export { default as SnapshotInterface } from "./Snapshot.js";
