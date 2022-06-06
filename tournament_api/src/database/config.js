8cc43e52cde2eeccfb37b315e9d84ed6599548f0
kKar1503 <isonice4572@gmail.com>
2022-06-06 22:21:32 +0800

    fix: :bug: trying insercure auth fix

--- tournament_api/src/database/config.js
+++ tournament_api/src/database/config.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { createConnection } from "mysql2";
+import { createConnection } from "mysql";
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS } = process.env;
 
 const dbConnection = {
@@ -10,21 +10,7 @@ const dbConnection = {
 			user: DB_USER,
 			password: DB_PASS,
 			database: "tetralympic",
-			multipleStatements: true,
-			authSwitchHandler: function ({ pluginName, pluginData }, cb) {
-				if (pluginName === "ssh-key-auth") {
-					getPrivateKey((key) => {
-						const response = encrypt(key, pluginData);
-						// continue handshake by sending response data
-						// respond with error to propagate error to connect/changeUser handlers
-						cb(null, response);
-					});
-				} else {
-					const err = new Error(`Unknown AuthSwitchRequest plugin name ${pluginName}`);
-					err.fatal = true;
-					cb(err);
-				}
-			},
+			insecureAuth: true,
 		});
 	},
 };
