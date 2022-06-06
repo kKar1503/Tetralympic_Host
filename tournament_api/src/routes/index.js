83d56971de44b977c9de773f5641e9afea893006
kKar1503 <isonice4572@gmail.com>
2022-06-06 21:14:32 +0800

    feat: :art: Combined auth server into one single port

--- tournament_api/src/routes/index.js
+++ tournament_api/src/routes/index.js
@@ -1,13 +1,34 @@
+import "dotenv/config";
 import express from "express";
 import registration from "./registration.js";
 import tetrio from "./tetrio.js";
 import discord from "./discord.js";
 import competition from "./competition.js";
+import jwt from "jsonwebtoken";
+const { SECRET_KEY } = process.env;
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
 	res.json({ message: "This is the home page of Tetralympic API!" });
+});
+
+router.post("/authenticate", (req, res) => {
+	let getTetrioUsers =
+		typeof req.body.getTetrioUsers === "boolean" ? req.body.getTetrioUsers : false;
+	let getDiscordUser =
+		typeof req.body.getDiscordUser === "boolean" ? req.body.getDiscordUser : false;
+	let pushSnapshots =
+		typeof req.body.pushSnapshots === "boolean" ? req.body.pushSnapshots : false;
+	let accessToken = jwt.sign(
+		{
+			getTetrioUsers: getTetrioUsers,
+			getDiscordUser: getDiscordUser,
+			pushSnapshots: pushSnapshots,
+		},
+		SECRET_KEY
+	);
+	res.json({ accessToken: accessToken });
 });
 
 router.use("/competition", competition);
